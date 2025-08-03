2b6f0f837cd8cea63eeba7fe932f1d84d1d3c0d6
Rhythem <rhythem.pradhan@connect.qut.edu.au>
2025-08-03 19:38:20 +1000

    taskController.js file is added

---
+++ backend/controllers/taskController.js
@@ -0,0 +1,58 @@
+const Task = require('../models/Task');
+
+// Get all tasks
+const getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find();
+    res.status(200).json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Add a new task
+const addTask = async (req, res) => {
+  const task = new Task({
+    title: req.body.title,
+    description: req.body.description
+  });
+
+  try {
+    const newTask = await task.save();
+    res.status(201).json(newTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Update a task
+const updateTask = async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedTask);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+// Delete a task
+const deleteTask = async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: 'Task deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+// Export all functions
+module.exports = {
+  getTasks,
+  addTask,
+  updateTask,
+  deleteTask
+};
